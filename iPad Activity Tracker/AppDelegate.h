163098e44eef2113ac8fdeb3720fff9535bc421d
David Van Puyvelde <dvpuyvelde@DVP.internal.salesforce.com>
2011-11-15 21:03:41 +0100

    Initial Commit

---
+++ iPad Activity Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPad Activity Tracker
+//
+//  Created by David Van Puyvelde on 15/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ iPad Activity Tracker/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  iPad Activity Tracker
+//
+//  Created by David Van Puyvelde on 15/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
